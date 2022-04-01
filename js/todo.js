cab81be3c1fd8f18e061c2cce1f146554b0dc949
Gihoon Lim <knv429@naver.com>
2022-04-01 22:30:02 +0900

    (#5) Todo
    delete

--- js/todo.js
+++ js/todo.js
@@ -14,13 +14,19 @@ function saveToDos() {
 function deleteToDo(event) {
   //target=button / button의 parent는? li
   const li = event.target.parentElement;
+  // li.id = string, toDo.id = Int 서로 type이 다르기 때문에 parseInt를 해주지 않으면 작동하지 않음
+  // li.id가 string인 이유는 dom이 li의 id를 string으로 자동 변환해서 받아들이기 때문
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   li.remove();
+  saveToDos();
+  // filter를 통해 조건에 맞는 것들만 골라서 새롭게 배열을 만들고 마지막에 saveToDos function을 사용해 localStorage에 덮어씌움
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "X";
   button.addEventListener("click", deleteToDo);
@@ -33,8 +39,12 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newTodo);
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
